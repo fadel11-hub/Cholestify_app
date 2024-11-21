1a0755671bac5f29e42b9f982297f7f212d80ab9
Fadel <fadelamili11@gmail.com>
2024-11-21 22:54:17 +0700

    Add file : 1.response 2.retrofit = ApiConfig.kt & ApiService.kt 3.Library retrofit dan Goso
    Fix
    1. Design ProfileFragment
    2. Design fragment_profile.xml
    3. MainActivity.kt
    4. fix HomeFragment.kt

---
+++ app/src/main/java/com/example/cholestifyapp/data/retrofit/ApiService.kt
@@ -0,0 +1,12 @@
+package com.example.cholestifyapp.data.retrofit
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+//    Link API Consume
+
+}
