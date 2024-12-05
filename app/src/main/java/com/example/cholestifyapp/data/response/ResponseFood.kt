a63c015f770031e0d8a702125cc3b3c6e2b1b2e6
Fadil0107 <fadilh0107@gmail.com>
2024-12-05 15:01:21 +0700

    merubah format any, menjadi lebih spesifik

--- app/src/main/java/com/example/cholestifyapp/data/response/ResponseFood.kt
+++ app/src/main/java/com/example/cholestifyapp/data/response/ResponseFood.kt
@@ -1,6 +1,7 @@
 package com.example.cholestifyapp.data.response
 
 import com.google.gson.annotations.SerializedName
+import java.util.Date
 
 data class ResponseFood(
 
@@ -17,22 +18,22 @@ data class ResponseFood(
 data class DataItem(
 
 	@field:SerializedName("potassium")
-	val potassium: Any? = null,
+	val potassium: Double? = null,
 
 	@field:SerializedName("manganese")
 	val manganese: Int? = null,
 
 	@field:SerializedName("vitaminB1")
-	val vitaminB1: Any? = null,
+	val vitaminB1: Double? = null,
 
 	@field:SerializedName("createdAt")
-	val createdAt: String? = null,
+	val createdAt: String? = null,  // Format tanggal bisa dikendalikan lebih lanjut jika diperlukan
 
 	@field:SerializedName("vitaminB2")
-	val vitaminB2: Any? = null,
+	val vitaminB2: Double? = null,
 
 	@field:SerializedName("vitaminB3")
-	val vitaminB3: Any? = null,
+	val vitaminB3: Double? = null,
 
 	@field:SerializedName("dietaryFiber")
 	val dietaryFiber: Int? = null,
@@ -44,89 +45,89 @@ data class DataItem(
 	val vitaminB5: Int? = null,
 
 	@field:SerializedName("vitaminB6")
-	val vitaminB6: Any? = null,
+	val vitaminB6: Double? = null,
 
 	@field:SerializedName("protein")
-	val protein: Any? = null,
+	val protein: Double? = null,
 
 	@field:SerializedName("fat")
-	val fat: Any? = null,
+	val fat: Double? = null,
 
 	@field:SerializedName("saturatedFats")
-	val saturatedFats: Any? = null,
+	val saturatedFats: Double? = null,
 
 	@field:SerializedName("polyunsaturatedFats")
-	val polyunsaturatedFats: Int? = null,
+	val polyunsaturatedFats: Double? = null,
 
 	@field:SerializedName("cholesterol")
 	val cholesterol: Int? = null,
 
 	@field:SerializedName("copper")
-	val copper: Any? = null,
+	val copper: Double? = null,
 
 	@field:SerializedName("vitaminB12")
-	val vitaminB12: Any? = null,
+	val vitaminB12: Double? = null,
 
 	@field:SerializedName("id")
 	val id: Int? = null,
 
 	@field:SerializedName("vitaminB11")
-	val vitaminB11: Any? = null,
+	val vitaminB11: Double? = null,
 
 	@field:SerializedName("zinc")
-	val zinc: Any? = null,
+	val zinc: Double? = null,
 
 	@field:SerializedName("phosphorus")
-	val phosphorus: Any? = null,
+	val phosphorus: Double? = null,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any? = null,
+	val updatedAt: String? = null,  // Format tanggal bisa dikendalikan lebih lanjut jika diperlukan
 
 	@field:SerializedName("carbohydrates")
-	val carbohydrates: Any? = null,
+	val carbohydrates: Double? = null,
 
 	@field:SerializedName("sugars")
 	val sugars: Int? = null,
 
 	@field:SerializedName("calcium")
-	val calcium: Any? = null,
+	val calcium: Double? = null,
 
 	@field:SerializedName("vitaminC")
-	val vitaminC: Any? = null,
+	val vitaminC: Double? = null,
 
 	@field:SerializedName("vitaminE")
-	val vitaminE: Any? = null,
+	val vitaminE: Double? = null,
 
 	@field:SerializedName("monounsaturatedFats")
-	val monounsaturatedFats: Any? = null,
+	val monounsaturatedFats: Double? = null,
 
 	@field:SerializedName("vitaminD")
 	val vitaminD: Int? = null,
 
 	@field:SerializedName("magnesium")
-	val magnesium: Any? = null,
+	val magnesium: Double? = null,
 
 	@field:SerializedName("vitaminK")
-	val vitaminK: Any? = null,
+	val vitaminK: Double? = null,
 
 	@field:SerializedName("nutritionDensity")
-	val nutritionDensity: Any? = null,
+	val nutritionDensity: Double? = null,
 
 	@field:SerializedName("water")
-	val water: Any? = null,
+	val water: Double? = null,
 
 	@field:SerializedName("food")
 	val food: String? = null,
 
 	@field:SerializedName("sodium")
-	val sodium: Any? = null,
+	val sodium: Double? = null,
 
 	@field:SerializedName("caloricValue")
 	val caloricValue: Int? = null,
 
 	@field:SerializedName("iron")
-	val iron: Any? = null,
+	val iron: Double? = null,
 
 	@field:SerializedName("vitaminA")
-	val vitaminA: Any? = null
+	val vitaminA: Double? = null
 )
